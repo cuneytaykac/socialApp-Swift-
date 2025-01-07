6b6d42a872c3cde5ec45f2c389842082d061880b
cuneyt.aykac <cuneyt.aykac@duzey.com.tr>
2025-01-07 21:12:23 +0300

    Initial Commit

---
+++ socialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  socialApp
+//
+//  Created by Cüneyt Aykaç on 7.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
